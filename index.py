2bc4a5be96caef3ab3a1314c3b4cd8f5b6e1fd50
Sujan Budhathoki <tsuzanbt@gmail.com>
2024-02-23 11:56:06 +0545

    initial project

---
+++ index.py
@@ -0,0 +1,26 @@
+
+from flask import Flask, jsonify, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def fetch_google():
+    body = request.files.get("fike")
+    return jsonify({"file": "test"})
+
+
+
+@app.route('/fetch_google', methods=['GET'])
+def fetch_google():
+    # Replace 'https://www.google.com' with the desired URL
+    url = 'https://www.google.com'
+    
+    # Make a GET request to the URL
+    response = requests.get(url)
+    
+    # Return the response content as JSON
+    return jsonify(content=response.text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
